af076e1d07bc1732f5e37aaa67ff25cf3bbded08
darul75 <darul75@gmail.com>
2014-01-08 14:14:59 +0100

    few tries

--- src/agent007.js
+++ src/agent007.js
@@ -7,13 +7,28 @@ var xml2js = require('xml2js');
 */
 
 var xml = 'useragentswitcher.xml';
-var LegacyBrowsers = 'Legacy Browsers';
-var ConsoleBrowsers = 'Console Browsers';
+
+// TYPES
+var Bill = 'Windows';
+var Steve = 'Mac';
+var Lyn = 'Linux';
+var Unix = 'Unix';
+var Mobile = 'MobileDevices';
+
+// SUB TYPES
+var LegacyBrowsers = 'LegacyBrowsers';
+var ConsoleBrowsers = 'ConsoleBrowsers';
+
+// MOBILE TYPES
 var Browsers = 'Browsers';
 var Devices = 'Devices';
+var Tablets = 'Tablets';
+var TabletsApple = 'AppleiPhoneetc';
 var OS = 'OS';
+
 var key = 0;
 
+var useragents = [];
 var node = [];
 
 function Agent007() {
@@ -21,19 +36,56 @@ function Agent007() {
 }
 
 Agent007.prototype.init = function() {
-  var parser = new xml2js.Parser();
-  var useragents = '';
+  var parser = new xml2js.Parser();  
 
   var _this = this;
 
   // http://techpatterns.com/downloads/firefox/useragentswitcher.xml
   fs.readFile(xml, function(err, data) {
       parser.parseString(data, function (err, result) {
-          //_this.useragents = result;
+          
           _this.index = new Index();
+          _this.index2 = new Index();
           _this.nodeIt(result.useragentswitcher.folder, node, true);
 
-          var results = _this.query("Firefox");
+          var results;
+
+          console.log("-------- mac legacy browsers -----------");
+          results = _this.query(Steve + LegacyBrowsers);
+        
+          for (var i=0;i<results.length;i++) {
+            console.log(useragents[results[i].key]);
+          }
+
+          console.log("\n-------- Mac -----------\n");
+          results = _this.query(Steve);
+
+          for (var i=0;i<results.length;i++) {
+            console.log(useragents[results[i].key]);
+          }
+
+          console.log("\n-------- Mobile -----------\n");
+          results = _this.query(Mobile+Browsers);
+
+          for (var i=0;i<results.length;i++) {
+            console.log(useragents[results[i].key]);
+          }
+
+          console.log("\n-------- Mobile/Tablets -----------\n");
+          results = _this.query(Mobile+Devices+Tablets);
+
+          for (var i=0;i<results.length;i++) {
+            console.log(useragents[results[i].key]);
+          }
+
+          console.log("\n-------- Mobile/Tablets/Apple -----------\n");
+          results = _this.query(Mobile+Devices+TabletsApple);
+
+          for (var i=0;i<results.length;i++) {
+            console.log(useragents[results[i].key]);
+          }
+
+          
 
           console.log("loaded");
 
@@ -43,8 +95,12 @@ Agent007.prototype.init = function() {
   return this;
 };
 
-Agent007.prototype.query = function(pattern) {
-  return this.index.query(pattern);
+Agent007.prototype.queryAgent = function(agent) {
+  return this.index.query(type);
+};
+
+Agent007.prototype.queryPath = function(type) {  
+  return this.index2.query(type);
 };
 
 Agent007.prototype.nodeIt = function(folders, node, root) {  
@@ -53,11 +109,13 @@ Agent007.prototype.nodeIt = function(folders, node, root) {
     return;
   for (var i=0;i<folders.length;i++) {
     var folder = folders[i];
-    var description = folder.$.description;
+    var description = folder.$.description.replace('Browsers - ', '').replace(' ', '').replace(')', ' ').replace('(', '');
     if (description)
     {
       var o = {name: description};
-      o.path = node.path ? node.path + ' / ' + description : description;
+      o.path = node.path ? node.path + '/' + description : description;
+      var compactDesc = description.replace(' ', '');
+      o.index = node.index ? node.index + compactDesc : compactDesc;
 
       this.fetchUserAgents(folder.useragent, o);
 
@@ -90,11 +148,16 @@ Agent007.prototype.fetchUserAgents = function(ua, o) {
       path: o.path      
     });
 
-    this.index.addDocument(key, {
-      path: o.path,
-      description: desc,
+    this.index.addDocument(key, {      
       userAgent: useragent
     });
+
+    this.index2.addDocument(key, {
+      path: o.index      
+    });
+
+    useragents[key] = useragent;
+
     key++;
   }
 
